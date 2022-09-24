8837da862ed98c80f752a9c5e01af4b9ed6f2b6a
Anubhav Arora <maxi@Anubhavs-MacBook-Pro.local>
2022-09-24 21:54:29 +0530

    modified create todo method to use fetch for async calls, also added try, except and finally clause to handle errors.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from asyncio import constants
+from flask import Flask, jsonify, render_template, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import false
 from flask_migrate import Migrate
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://maxi@127.0.0.1:5432/todowebapp'
@@ -16,18 +18,35 @@ class ToDo(db.Model):
     description = db.Column(db.String(), nullable = False)
     completed = db.Column(db.Boolean, nullable = False, default = False)
 
+    def __repr__(self) -> str:
+        return f'<ToDo: {self.id} {self.description} {self.completed}>'
+
 @app.route('/')
-def index(): #home route handler
+def index(): #route handler
     return render_template('index.html', todos = ToDo.query.order_by('id').all())
 
 @app.route('/todos/create', methods = ['POST'])
 def create_todo():
-    todo_description = request.form.get('description')
-    new_todo = ToDo(description = todo_description)
-    print(todo_description)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for('index'))
+    error = False
+    body = {}
+    try:
+        todo_description = request.get_json()['description']
+        new_todo = ToDo(description = todo_description)
+        db.session.add(new_todo)
+        db.session.commit()
+        body['description'] = todo_description
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    
+    if error:
+        abort(400)
+    else:
+        return jsonify(body)
+
 
 if __name__ == '__main__':
-    app.run(debug = True, port = 3000)
+    app.run(debug = True, port = 5000)
